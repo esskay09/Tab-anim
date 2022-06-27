3267210b45f592c84248a908c4faeb584d64bc1c
Shaique <esskay099@gmail.com>
2022-06-27 20:26:55 +0530

    Polish all UI

---
+++ app/src/main/java/com/terranullius/exampleproject/presentation/top_bar/NonSearchContent.kt
@@ -0,0 +1,70 @@
+package com.terranullius.exampleproject.presentation.top_bar
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+import com.terranullius.exampleproject.presentation.components.OptionsOverflowComposable
+import com.terranullius.exampleproject.presentation.components.OverFlowAction
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun NonSearchContent(
+    modifier: Modifier = Modifier,
+    pagerState: PagerState,
+    onSearchClick: () -> Unit = {}
+) {
+    Column(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 6.dp)
+        ) {
+            Text(
+                text = "Sharma Store",
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(5.dp),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colors.onPrimary
+            )
+
+            Row() {
+                Icon(
+                    Icons.Outlined.Search,
+                    contentDescription = "",
+                    tint = MaterialTheme.colors.secondary,
+                    modifier = Modifier.clickable {
+                        onSearchClick()
+                    })
+
+                Spacer(modifier = Modifier.width(6.dp))
+
+                OptionsOverflowComposable(
+                    actions = remember {
+                        List(4) {
+                            OverFlowAction(name = "Option $it") {}
+                        }
+                    }
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        TabLayout(pagerState)
+    }
+
+}
---
+++ app/src/main/java/com/terranullius/exampleproject/presentation/top_bar/SearchContent.kt
@@ -0,0 +1,52 @@
+package com.terranullius.exampleproject.presentation.top_bar
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun SearchContent(modifier: Modifier, onBackClick: () -> Unit) {
+
+    var searchValue by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = modifier) {
+        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                Icons.Outlined.ArrowBack,
+                contentDescription = "",
+                tint = MaterialTheme.colors.primary,
+                modifier = Modifier.clickable {
+                    onBackClick()
+                })
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = searchValue,
+                placeholder = {
+                    Text(text = "Search...", color = Color.Gray)
+                },
+                onValueChange = { searchValue = it },
+                textStyle = MaterialTheme.typography.body1.copy(
+                    color = MaterialTheme.colors.primary
+                )
+            )
+        }
+
+        Text(
+            text = "Placeholder data",
+            modifier = Modifier.padding(vertical = 25.dp, horizontal = 10.dp)
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+        Divider(thickness = 3.dp, color = Color.Gray)
+    }
+}
